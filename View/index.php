6c5a40b829ed6734f78fe3db236e36d2ee8f7870
Lucas Alves <lucas1alves98@gmail.com>
2021-02-15 16:54:09 -0300

    header finalizado

--- View/index.php
+++ View/index.php
@@ -1,15 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- links para funcionamento dos icons retirados do site Favicon.io-->
+    <link rel="apple-touch-icon" sizes="180x180" href="img/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/icons/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <!-- Links para as fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Da+2:wght@400;500;600;700&family=Josefin+Slab:ital,wght@0,300;0,600;1,300;1,400;1,600&family=Mulish:ital,wght@0,200;0,300;0,400;0,500;1,200;1,300;1,400&display=swap" rel="stylesheet">
+
+    <!-- Links para css-->
+    <link rel="stylesheet" href="css/style.css">
+
     <title>Document</title>
 </head>
 
 <body>
-    dsad
+    <div class="container">
+        <header>
+            <div class="img-wrapper">
+                <img src="img/bg.jpg" alt="">
+            </div>
+            <div class="banner">
+                <h1>Web developer e Front End Designer</h1>
+                <p>Lucas Alves de Carvalho</p>
+                <button>Saiba Mais</button>
+            </div>
+        </header>
+    </div>
 </body>
 
 </html>
